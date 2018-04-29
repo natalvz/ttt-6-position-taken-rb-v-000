a8fff8f5f3ea53a5b9c72ed477be0a20b2ef2a47
Natalia Castro <nat.alvz@gmail.com>
2018-04-29 01:19:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,4 +3,10 @@ def position_taken?(board, index)
   if array[index_number] == " " 
     return false
   end
+  if array[index_number] == "" 
+    return false
+  end
+  if array[index_number] == nil 
+    return false
+  end
 end
