2efaae7872e8a46ed3c1b6b12f1c0bca8fc2c4a9
Natalia Castro <nat.alvz@gmail.com>
2018-04-29 01:13:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(board,index)
+def position_taken?(board[index])
   if array[index_number] == " " || array[index_number] == "" 
     return false
   end
